d8ee9b66795b0b919a089eebaf23643659964cf8
jenniferom <jenniferom0009@gmail.com>
2024-09-21 22:24:27 -0400

    Curso Clase 1, añadiendo componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/button/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleClick = () => {
+    alert("Click Button 1");
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <Button text="Button 1" onClick={handleClick}/>
+      <Button text="Button 2" onClick={() => alert('Click Button 2')} disabled/>
+      <Button text="Button 3" onClick={() => alert('Click Button 3')}/>
+      <Button text="Button 4" onClick={() => alert('Click Button 4')}/>
+    </div>
+  );
 }
 
 export default App
